9848c61bbedea7154ba0c732662a1497325d9c5f
Yasin Karaca <yasin.karaca@hetic.net>
2021-02-01 15:51:16 +0100

    get data and raw display

---
+++ src/components/Card.vue
@@ -0,0 +1,44 @@
+<template>
+  <article>
+      <a :href='urlArticle' target='_blank'>
+        <figure>
+          <img :src='thumbnail' :alt='altImage'>
+          <figcaption>{{data.folder.name}}</figcaption>
+        </figure>
+      </a>
+  </article>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+
+        }
+    },
+    props: {
+        data: Object
+    },
+    computed: {
+        thumbnail: {
+            get: function () {
+                return this.data.thumbnail.URL.replace('{size}', 'thumb');
+            }
+        },
+        altImage: {
+            get: function () {
+                return this.data.thumbnail.title;
+            }
+        },
+        urlArticle: {
+            get: function () {
+                return this.data.URL;
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Card v-for="( item, index ) in cards" :key="index" :data="item"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
+import Card from '@/components/Card.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Card
+  },
+  data: () => {
+    return {
+      apiUrl: 'https://api.melty.fr/v1/fr/feeds/2',
+      errorData: false,
+      articles: [],
+      numberOfCards: 6
+    }
+  },
+  created: function() {
+    axios.get(this.apiUrl)
+      .then((response) => {
+        this.cards = response.data.feed_elements.items.slice(0, this.numberOfCards);
+      })
+      .catch((error) => {
+        this.errorData = true;
+        console.error(error);
+      })
+  },
+  computed: {
+    cards: {
+      get: function () {
+        return this.articles;
+      },
+      set: function (v) {
+        this.articles = v;
+      }
+    }
   }
 }
 </script>
